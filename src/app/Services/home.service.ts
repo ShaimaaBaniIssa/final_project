406989fddeab9fb128f7a403fe13501fab50aa4e
Mohammadalomari01 <mohammad5112001@gmail.com>
2024-10-13 04:09:01 +0300

    Home Page DynmicData Part1 :)

--- src/app/Services/home.service.ts
+++ src/app/Services/home.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class HomeService {
 
-  constructor() { }
+  constructor(public http:HttpClient) {
+    // api code
+   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  projectname="Train Tracker";
+ name="Foryraa";
+ image ="../../assets/Home/img/Foryra1.JPG";
+ postion = "Full Stack Developer";
+ personalText ="I’ll will help you to build strategy from scratch till deliver";
+ station="Irbid";
+ stationimg="../../assets/Home/img/Irbid_Station.jpg";
+ numofdes="86 Destinations";
+ trainlogo="../../assets/Home/img/train_logo.png"
+ WebsiteTitle="Discover New Destinations, One Train Journey at a Time with Train Tracker!";
+ toptext="Experience Tailored Train Journeys and Exceptional Service. Explore New Destinations Without Breaking the Bank!";
+ TitileAbouttext="Stay on Track with Train Tracker – Your Ultimate Guide to Seamless Train Journeys!";
+ WelcomeText="Triptopia was born out of a shared passion for exploration and a desire to create meaningful travel experiences. We believe travel is more than just visiting a place";
+ welcomeimage1="../../assets/Home/img/Station1.jpg";
+ desttitle="Discover the best places in the world to visit by train.";
+ desttext="Our travel platform offers a range of features that make planning your journey simple and enjoyable. With our advanced search and booking tools, finding the perfect train route has never been easier.";
 }
