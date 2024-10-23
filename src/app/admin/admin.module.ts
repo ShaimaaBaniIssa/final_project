4951c2cc9112b982e5ab258abbb5458399c5946a
shahedalserhan <shahedalserhan20@gmail.com>
2024-10-23 19:39:46 +0300

    html

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { DashbordComponent } from './dashbord/dashbord.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
-    DashbordComponent
+    DashbordComponent,
+    SidebarComponent
   ],
   imports: [
     CommonModule,
